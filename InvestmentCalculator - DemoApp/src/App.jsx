a487acd6503561f274199829eb9b3945cf38ac6b
In Tae Chung <intae29@gmail.com>
2024-10-13 13:34:55 -0400

    incorporated calculateInvestmentResults to receive user input fields then return object with correct calculations

--- InvestmentCalculator - DemoApp/src/App.jsx
+++ InvestmentCalculator - DemoApp/src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import ResultsTable from "./components/ResultsTable";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 100,
+    expectedReturn: 5,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        // Having a '+' in front of newValue forces the conversion of the string value to be treated as a number value
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange}/>
+      <ResultsTable input={userInput} />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ InvestmentCalculator - DemoApp/src/components/ResultsTable.jsx
@@ -0,0 +1,13 @@
+import { calculateInvestmentResults } from '../util/investment.js'
+
+export default function ResultsTable({ input }) {
+    const resultsData = calculateInvestmentResults(input);
+
+    console.log(resultsData)
+
+    return (
+        <>
+            Results...
+        </>
+    )
+}
