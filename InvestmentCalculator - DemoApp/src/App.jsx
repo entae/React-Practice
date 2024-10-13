6cdfd6ee9e79033f8fb64b894efc559f480449da
In Tae Chung <intae29@gmail.com>
2024-10-13 13:52:49 -0400

    Added Error message and conditional display of table when duration is less than 1

--- InvestmentCalculator - DemoApp/src/App.jsx
+++ InvestmentCalculator - DemoApp/src/App.jsx
@@ -12,6 +12,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -25,8 +27,12 @@ function App() {
   return (
     <>
       <Header />
-      <UserInput userInput={userInput} onChange={handleChange}/>
-      <ResultsTable input={userInput} />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid ? (
+        <p className="center">Please enter a duration greater than 0</p>
+      ) : (
+        <ResultsTable input={userInput} />
+      )}
     </>
   );
 }
