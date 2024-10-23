2c2429618d9a30ad78d02af70f29237d986436ca
In Tae Chung <intae29@gmail.com>
2024-10-23 19:10:25 -0400

    added score if timer was successfully stopped and displayed conditional modal

--- refsAndPortals/src/components/ResultModal.jsx
+++ refsAndPortals/src/components/ResultModal.jsx
@@ -1,15 +1,19 @@
-import { useRef } from "react";
-import { forwardRef, useImperativeHandle } from "react";
+import { useRef, forwardRef, useImperativeHandle } from "react";
 
 // forwardRef must be used to pass on ref as a prop to a custom component
 // the entire function will be wrapped in the forwardRef function and then exported as a constant variable
 // the ref value is passed on as a second argument outside the component's props
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100  )// score between 1-100
+
   useImperativeHandle(ref, () => {
     return {
       open() {
@@ -20,14 +24,15 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime}</strong>.
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
