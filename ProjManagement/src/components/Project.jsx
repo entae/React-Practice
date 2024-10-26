5544f77f0ab55ab54d84dd8791913b43483ae222
In Tae Chung <intae29@gmail.com>
2024-10-26 17:18:52 -0400

    ProjManagement/ Can now add or clear tasks to each project

--- ProjManagement/src/components/Project.jsx
+++ ProjManagement/src/components/Project.jsx
@@ -1,4 +1,4 @@
-import Task from "./Task";
+import Task from "./Tasks";
 
 export default function Project({name}) {
     return (
--- ProjManagement/src/components/Task.jsx
+++
@@ -1,8 +0,0 @@
-export default function Task({ name, ...props }) {
-    return(
-        <>
-            <h2>{name}</h2>
-            <button>Clear</button>
-        </>
-    )
-}
