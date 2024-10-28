fec69fe7c1151b0ea6300a9afabbddb775ac9b49
In Tae Chung <intae29@gmail.com>
2024-10-28 19:51:35 -0400

    ShoppingCart/ Moved logic for context management into shopping-cart-context file to streamline App.jsx file

--- ShoppingCart/src/store/shopping-card-context.jsx
+++ ShoppingCart/src/store/shopping-card-context.jsx
@@ -1,11 +1,86 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+
+import { DUMMY_PRODUCTS } from "../dummy-products";
 
 // Starts with a capital letter bc it's object that contains a React component
 export const CartContext = createContext({
   items: [],
-  
+
   // doesn't actually get used, but helps with autocompletion
-  addItemToCart: () => {}, 
-  updateItemQuantity: ()=> {},
+  addItemToCart: () => {},
+  updateItemQuantity: () => {},
 });
 
+// This function is all about managing context data about the shopping cart and providing that data to our application
+export default function CartContextProvider({children}) {
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
+  });
+
+  function handleAddItemToCart(id) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  const ctxValue = {
+    items: shoppingCart.items,
+    // any item that has this component can now call this function below
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
+  };
+
+  return <CartContext.Provider value={ctxValue}>
+    {children}
+  </CartContext.Provider>
+}
