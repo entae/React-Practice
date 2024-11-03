6f5bc6cbf2690146afe8766c9ccf802dc8286574
In Tae Chung <intae29@gmail.com>
2024-11-03 12:33:13 -0500

    PlacePicker/ added ProgressBar component to display timer on delete place modal

--- PlacePicker/src/components/DeleteConfirmation.jsx
+++ PlacePicker/src/components/DeleteConfirmation.jsx
@@ -1,15 +1,20 @@
 import { useEffect } from "react";
 
+import ProgressBar from "./ProgressBar";
+
+const TIMER = 3000;
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+
   useEffect(() => {
     console.log("TIMER SET");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
-    //clean up function
+    // "Clean up" function
     return () => {
-      console.log('Cleaning up timer')
+      console.log("Cleaning up timer");
       clearTimeout(timer);
     };
   }, [onConfirm]);
@@ -26,6 +31,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
