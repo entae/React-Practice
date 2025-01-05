a45a9debfec66cd30b6e3943107fecccca3f865b
In Tae Chung <intae29@gmail.com>
2025-01-05 12:50:33 -0500

    setting up new section: CustomReactHooks

---
+++ CustomReactHooks/src/hooks/useFetch.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+// convention that hooks need to start with "use-"
+function useFetch() {
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchUserPlaces();
+        setUserPlaces(places);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch user places." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+}
