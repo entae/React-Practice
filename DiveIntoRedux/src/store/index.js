a155ebd6436168d85a92bce1e1dd7910e5be41d8
In Tae Chung <intae29@gmail.com>
2025-02-07 15:18:09 -0500

    DiveIntoRedux/ used Prettier on components/Counter.js

--- DiveIntoRedux/src/store/index.js
+++ DiveIntoRedux/src/store/index.js
@@ -21,50 +21,48 @@ const counterSlice = createSlice({
     toggleCounter(state) {
       state.showCounter = !state.showCounter;
     },
-  }
+  },
 });
 
-
 // const counterReducer = (state = initialState, action) => {
-  //   if (action.type === "increment") {
-    //     return {
-      //       counter: state.counter + 1,
-      //       showCounter: state.showCounter,
-      //     };
-      //   }
-      
-      //   if (action.type === "increase") {
-        //     return {
-          //       counter: state.counter + action.amount,
-          //       showCounter: state.showCounter,
-          //     };
-          //   }
-          
-          //   if (action.type === "decrement") {
-            //     return {
-              //       counter: state.counter - 1,
-              //       showCounter: state.showCounter,
-              //     };
-              //   }
-              
-              //   if (action.type === "toggle") {
-                //     return {
-                  //       showCounter: !state.showCounter,
-                  //       counter: state.counter,
-                  //     };
-                  //   }
-                  
-                  //   return state;
-                  // };
-                  
-                  const store = configureStore({
-                    reducer: counterSlice.reducer,
-                    // reducer: { counter: counterSlice.reducer }
-                  });
-                  
-                  // Methods on the actions object will create action objects for us
-                  // "action creators" = already have a type property with a unique identifier
-                  export const counterActions = counterSlice.actions;
-                  
-                  export default store;
-                  
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+
+//   return state;
+// };
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+  // reducer: { counter: counterSlice.reducer }
+});
+
+// Methods on the actions object will create action objects for us
+// "action creators" = already have a type property with a unique identifier
+export const counterActions = counterSlice.actions;
+
+export default store;
